c6ed410d7622d5f186790ba3b6a621e457c0ec4e
James Reeves <jreeves@weavejester.com>
2023-05-07 19:51:13 +0100

    Add basic Reedline prompt

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use reedline::{DefaultPrompt, DefaultPromptSegment, Reedline, Signal};
+
 fn main() {
-    println!("Hello, world!");
+    let mut line_editor = Reedline::create();
+    let prompt = DefaultPrompt::new(
+        DefaultPromptSegment::Empty,
+        DefaultPromptSegment::Empty,
+    );
+
+    loop {
+        let sig = line_editor.read_line(&prompt).unwrap();
+        match sig {
+            Signal::Success(buffer) => {
+                println!("We processed: {}", buffer);
+            }
+            Signal::CtrlD | Signal::CtrlC => {
+                break;
+            }
+        }
+    }
 }
