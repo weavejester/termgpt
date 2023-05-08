92d8f481da9b86fcc8b52cdfcca8a69476441d29
James Reeves <jreeves@weavejester.com>
2023-05-08 14:56:22 +0100

    Wrap messages in container trait

--- src/main.rs
+++ src/main.rs
@@ -52,6 +52,22 @@ async fn get_chatgpt_response(
     Ok(response)
 }
 
+trait ChatMessageContainer {
+    fn messages(&self) -> &[ChatGptMessage];
+    fn push(&mut self, message: ChatGptMessage);
+}
+
+struct TransientChatMessages(Vec<ChatGptMessage>);
+
+impl ChatMessageContainer for TransientChatMessages {
+    fn messages(&self) -> &[ChatGptMessage] {
+        &self.0
+    }
+    fn push(&mut self, message: ChatGptMessage) {
+        self.0.push(message);
+    }
+}
+
 fn termimad_skin() -> MadSkin {
     let mut skin = MadSkin::default_dark();
     skin.paragraph.set_fg(Color::AnsiValue(249));
@@ -59,13 +75,15 @@ fn termimad_skin() -> MadSkin {
 }
 
 #[tokio::main]
-async fn main_loop(api_key: &str, model: &str) -> Result<(), Box<dyn Error>> {
+async fn main_loop<T: ChatMessageContainer>(
+    api_key: &str,
+    model: &str,
+    mut messages: T,
+) -> Result<(), Box<dyn Error>> {
     let mut line_editor = Reedline::create();
     let prompt = DefaultPrompt::new(Empty, Empty);
 
     let term_skin = termimad_skin();
-
-    let mut messages: Vec<ChatGptMessage> = Vec::new();
 
     loop {
         let sig = line_editor.read_line(&prompt)?;
@@ -75,7 +93,9 @@ async fn main_loop(api_key: &str, model: &str) -> Result<(), Box<dyn Error>> {
 
                 let mut spinner = Spinner::new(Spinners::Dots2, String::new());
 
-                let resp = get_chatgpt_response(&api_key, model, &messages);
+                let resp = get_chatgpt_response(
+                    api_key, model, &messages.messages());
+
                 let mesg = resp.await?.choices.pop().unwrap().message;
 
                 spinner.stop_with_message(
@@ -110,5 +130,7 @@ fn main() -> Result<(), Box<dyn Error>> {
         .or(env::var("OPENAI_API_KEY").ok())
         .expect("OpenAI API key not set");
 
-    main_loop(&api_key, &args.model)
+    let messages = TransientChatMessages(Vec::new());
+
+    main_loop(&api_key, &args.model, messages)
 }
